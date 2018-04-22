107d35683736cba7934ef8d32cd98834bb262944
Diego Robles <diego@hemployeed.com>
2018-04-22 04:25:48 -0300

    added dependencies and basic project filestructure

---
+++ imports/startup/client/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Meteor } from "meteor/meteor";
+import { render } from "react-dom";
+
+Meteor.startup(() => {
+  render(<h1>Hello, world!</h1>, document.getElementById("react-root"));
+});
